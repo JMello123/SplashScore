0d936f66b4257987ec1ac8fc787791068d8082a0
juan <juan.rika123@gmail.com>
2020-07-10 03:57:10 -0300

    fixed screen savedScore with label duplicated

--- main.py
+++ main.py
@@ -9,15 +9,73 @@ import time
 
 class Manager(ScreenManager):
 
-    sessions = []
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.status = {
+            'point': 0,
+            'attempt':0,
+            'accuracy':.0,
+            'sequence':[0,0],
+            'best_sequence':0,
+            'worst_sequence':0 
+        }
+        self.data = []
+
+    def increment_point(self,*args):
+        self.status['point'] += 1
+        self.status['attempt'] += 1
+        self.status['accuracy'] = self.status['point']/self.status['attempt']
+        self.status['sequence'][0] += 1
+        self.status['sequence'][1] = 0
+        self.check_sequence()
+        text = str(self.status['point'])+ '/' +str(self.status['attempt']) + ' -> ' + str(round(self.status['accuracy']*100,1))+ '%'
+        print(self.status)
+        return text
+
+    def increment_error(self, *args):
+        self.status['attempt'] += 1
+        self.status['accuracy'] = self.status['point']/self.status['attempt']
+        self.status['sequence'][0] = 0
+        self.status['sequence'][1] += 1
+        self.check_sequence()
+        text = str(self.status['point'])+ '/' +str(self.status['attempt']) + ' -> ' + str(round(self.status['accuracy']*100,1))+ '%'
+        print(self.status)
+        return text
+        
+    def check_sequence(self):
+            if self.status['sequence'][0] > self.status['best_sequence']:
+                self.status['best_sequence'] = self.status['sequence'][0]
+            if self.status['sequence'][1] > self.status['worst_sequence']:
+                self.status['worst_sequence'] = self.status['sequence'][1]
+        
+    def reset_status(self):
+        self.status['point'] = 0
+        self.status['attempt'] = 0
+        self.status['accuracy'] = .0
+        
     def loadDrill(self,*args):
         path = App.get_running_app().user_data_dir+'/'
         try:
             with open(path+'saved_score.json','r') as saved_score:
-                self.sessions = json.load(saved_score)
-                return self.sessions
+                    self.data = json.load(saved_score)
+                    return self.data
+
         except FileNotFoundError:
             open(path+'saved_score.json','x')
+            return self.data
+        except json.JSONDecodeError:
+            return []
+        
+    def saveDrill(self, *args):
+        date = time.localtime()
+        saved_date = f'{date.tm_mday}/{date.tm_mon}/{date.tm_year} {date.tm_hour}:{date.tm_min}'
+        path = App.get_running_app().user_data_dir+'/'
+        self.data.append({'accuracy':self.status['accuracy'], 
+                          'points':self.status['point'], 
+                          'date':saved_date})
+        with open(path + 'saved_score.json', 'w') as saved_score:
+            json.dump(self.data, saved_score)
+        self.reset_status()
         
     
 class Menu(Screen):
@@ -25,78 +83,53 @@ class Menu(Screen):
 
 class Drill(Screen):
     
-    data = []
-
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        self.point = 0
-        self.attempt = 0
-        self.accuracy =.0
-
-    def reset_status(self):
-        self.point = 0
-        self.attempt = 0
-        self.accuracy =.0
-        self.ids.status.text = '0/0'
-
     def on_pre_enter(self):
         Window.bind(on_keyboard=self.back_menu)
-        
-        self.data = App.get_running_app().root.loadDrill()
-        # print(path)
+        # App.get_running_app().root.loadDrill()
 
     def on_pre_leave(self):
         Window.unbind(on_keyboard=self.back_menu)
-
-    def saveDrill(self, *args):
-        date = time.localtime()
-        saved_date = f'{date.tm_mday}/{date.tm_mon}/{date.tm_year} {date.tm_hour}:{date.tm_min}'
-        path = App.get_running_app().user_data_dir+'/'
-        self.data.append({'accuracy':self.accuracy, 
-                          'points':self.point, 
-                          'date':saved_date})
-        with open(path + 'saved_score.json', 'w') as saved_score:
-            json.dump(self.data, saved_score)
-        self.reset_status()
+        self.ids.status.text = '0/0'
 
     def shot_splash(self, status):
-        self.point += 1
-        self.attempt += 1
-        self.accuracy = self.point/self.attempt
-        points = str(self.point)+'/'+str(self.attempt)+' -> '+str(round(self.accuracy*100,1))+'%'
-        status.text = points
+        points_text = App.get_running_app().root.increment_point()
+        status.text = str(points_text)
 
     def shot_wrong(self, status):
-        self.attempt += 1
-        self.accuracy = self.point/self.attempt
-        points = str(self.point)+'/'+str(self.attempt)+' -> '+str(round(self.accuracy*100,1))+'%'
-        status.text = points
+        points_text = App.get_running_app().root.increment_error()
+        status.text = str(points_text)
 
     def back_menu(self, window, key, *args):
             if key == 27:
                 App.get_running_app().root.current = 'menu'
+                self.ids.status.text = '0/0'
             return True
 
 
 class SavedScore(Screen):
 
-    data = []
     def on_pre_enter(self):
-        self.data = App.get_running_app().root.loadDrill()
-        for session in self.data:
+        self.ids.box.clear_widgets()
+        sessions = App.get_running_app().root.loadDrill()
+        print(sessions)
+        for session in sessions:
             self.ids.box.add_widget(SessionScore(session))
-            
-
+    
+    
 class SessionScore(BoxLayout):
 
     def __init__(self,session,**kwargs):
         super().__init__(**kwargs)
         self.ids.accuracy.text = str(round(session['accuracy']*100,1))+"%"
         self.ids.splash.text = str(session['points'])
-        self.ids.best_sequence.text = 'Nada, ainda!'
+        self.ids.best_sequence.text = 'Nada ainda'
         self.ids.date.text = str(session['date'])
 
+    
+
+
 class Splashscore(App):
+
     def build(self):
         return Manager()
 
